ca6434c0f561e500a8b3f575cfca4e9437357366
Tri M. Nguyen <mail@trimn.net>
2014-02-18 14:08:17 +0100

    Add country not found handling <3

--- olympics/Olympics.py
+++ olympics/Olympics.py
@@ -20,7 +20,7 @@ def get_medals_by_country(country):
     res = olympics.get_medals_based_on_country(country)
 
     if len(res) == 0:
-        return create_json_response({"error":"no data found"})
+        return create_json_response([{"c":country, "t": 0}])
     else:
         return create_json_response(olympics.get_medals_based_on_country(country))
 
